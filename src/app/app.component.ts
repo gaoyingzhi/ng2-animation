fa72bfdb38e63454d148613cb0e57ed7cba81895
gaoyingzhi <gao-yingzhi@yandex.com>
2017-05-16 19:57:26 +0300

    attach animation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,21 @@
 import { Component } from '@angular/core';
+import { trigger,state,style,transition,animate,keyframes } from '@angular/animations';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  animations: [
+    trigger('myAwesomeAnimation', [
+        state('small', style({
+            transform: 'scale(1)',
+        })),
+        state('large', style({
+            transform: 'scale(1.2)',
+        })),
+    ]),
+    transition('small => large', animate('100ms ease-in')),
+  ]
 })
 export class AppComponent {
   title = 'app works!';
